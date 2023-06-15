23d45f240196c96d6bc02512baf8624b102790c0
xixilili423 <1782532694@qq.com>
2023-06-15 11:42:10 +0800

    测试

---
+++ src/test/java/com/service/impl/UserServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void billing() {
+    }
+
+    @Test
+    void tracking() {
+    }
+
+    @Test
+    void getToken() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+}
